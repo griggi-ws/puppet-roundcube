a3e71bfe0b4d9ca5d5a345452593f4918b50d1f0
griggi-ws <gavin.riggi@wellspring.com>
2025-08-21 13:41:08 -0400

    change: use initdb.sh with wider support

--- files/initdb.sh
+++ files/initdb.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env php
 <?php
-
 /*
  +-----------------------------------------------------------------------+
  | This file is part of the Roundcube Webmail client                     |
@@ -19,18 +18,17 @@
  +-----------------------------------------------------------------------+
 */
 
-define('INSTALL_PATH', realpath(__DIR__ . '/..') . '/');
+define('INSTALL_PATH', '/var/lib/roundcube/');
 
 require_once INSTALL_PATH . 'program/include/clisetup.php';
 
 // get arguments
-$opts = rcube_utils::get_opt([
+$opts = rcube_utils::get_opt(array(
     'd' => 'dir',
-    'u' => 'update',
-]);
+));
 
 if (empty($opts['dir'])) {
-    rcube::raise_error('Database schema directory not specified (--dir).', false, true);
+    rcube::raise_error("Database schema directory not specified (--dir).", false, true);
 }
 
 // Check if directory exists
@@ -38,11 +36,4 @@ if (!file_exists($opts['dir'])) {
     rcube::raise_error("Specified database schema directory doesn't exist.", false, true);
 }
 
-$db = rcmail_utils::db();
-
-if (!empty($opts['update']) && in_array($db->table_name('system'), (array) $db->list_tables())) {
-    echo 'Checking for database schema updates...' . \PHP_EOL;
-    rcmail_utils::db_update($opts['dir'], 'roundcube', null, ['errors' => true]);
-} else {
-    rcmail_utils::db_init($opts['dir']);
-}
+rcmail_utils::db_init($opts['dir']);
